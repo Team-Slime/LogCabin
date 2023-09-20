f2ac28c88e6a4290d1b13ed5ff2b527dfb73d3e7
slimeyar <lostbhijaya@gmail.com>
2023-09-20 18:30:24 +0700

    Move out the function

--- markdown_parser.gd
+++ markdown_parser.gd
@@ -1,6 +1,33 @@
 class_name MdParser extends Node
 
 
+var header_six := RegEx.create_from_string("^\\#\\#\\#\\#\\#\\#(.*)*/gmi")
+var header_five := RegEx.create_from_string("^\\#\\#\\#\\#\\#(.*)*/gmi")
+var header_four := RegEx.create_from_string("^\\#\\#\\#\\#(.*)*/gmi")
+var header_three := RegEx.create_from_string("^\\#\\#\\#(.*)*/gmi")
+var header_two := RegEx.create_from_string("^\\#\\#(.*)*|(.*)(?:[\\n])-+/gmi")
+var header_one := RegEx.create_from_string("^\\#(.*)*|(.*)(?:[\\n])=+/gmi")
+
+var bold_italic := RegEx.create_from_string("\\*\\*\\*(.*)\\*\\*\\*|\\_\\_\\_(.*)\\_\\_\\_/gmi")
+var bold := RegEx.create_from_string("\\*\\*(.*)\\*\\|\\_\\_(.*)\\_\\_*/gmi")
+var italic := RegEx.create_from_string("\\*(.*)\\|\\_(.*)\\_*/gmi")
+
+var underline := RegEx.create_from_string("\\_(.*)\\_/gmi")
+var strikethrough := RegEx.create_from_string("\\~\\~(.*)\\~\\~/gmi")
+
+var superscript := RegEx.create_from_string("\\^(\\w*)/gmi")
+var subscript := RegEx.create_from_string("\\~(.*)\\~/gmi")
+
+var quote := RegEx.create_from_string("\\>(.*)/gmi")
+
+var horizontal_line := RegEx.create_from_string("\\-\\-\\-/gmi")
+
+var code_line := RegEx.create_from_string("\\`(.*)\\`/gmi")
+var code_block := RegEx.create_from_string("\\`\\`\\`(.*)\\`\\`\\`/gmi")
+
+var image := RegEx.create_from_string("!\\[([^\\]]+)\\]\\(([^)]+)\\)/gmi")
+var link := RegEx.create_from_string("\\[([^\\]]+)\\]\\(([^)]+)\\)/gmi")
+
 
 
 func _ready():
@@ -16,21 +43,5 @@ func _ready():
 
 func markdown_to_bbcode(markdown_text):
 	# https://github.com/feralhosting/feralhosting.github.io/blob/master/b2m/index.html
-	var header_six := RegEx.create_from_string("^\\#\\#\\#\\#\\#\\#(.*)*/gmi")
-	var header_five := RegEx.create_from_string("^\\#\\#\\#\\#\\#(.*)*/gmi")
-	var header_four := RegEx.create_from_string("^\\#\\#\\#\\#(.*)*/gmi")
-	var header_three := RegEx.create_from_string("^\\#\\#\\#(.*)*/gmi")
-	var header_two := RegEx.create_from_string("^\\#\\#(.*)*/gmi")
-	var header_one := RegEx.create_from_string("^\\#(.*)*/gmi")
 	
-	var bold_italic := RegEx.create_from_string("\\*\\*\\*(.*?)\\*\\*\\*/gmi")
-	var bold := RegEx.create_from_string("\\*\\*(.*?)\\*\\*/gmi")
-	var italic := RegEx.create_from_string("\\*(.*?)\\*/gmi")
 	
-	var underline := RegEx.create_from_string("_([^_]+)_/gmi")
-	var strikethrough := RegEx.create_from_string("~~([^~]+)~~/gmi")
-	
-	var code_line := RegEx.create_from_string("\\`(.*?)\\`/gmi")
-	var code_block := RegEx.create_from_string("\\`\\`\\`(.*?)\\`\\`\\`/gmi")
-	
-	var link := RegEx.create_from_string("\\[([^\\]]+)\\]\\(([^)]+)\\)/gmi")
