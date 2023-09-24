b5a1a4a081aa63e2eff97dae93660cad5572f92e
slimeyar <lostbhijaya@gmail.com>
2023-09-24 16:32:42 +0700

    More thing

--- markdown_parser.gd
+++ markdown_parser.gd
@@ -9,7 +9,7 @@ var header_two := RegEx.create_from_string("^\\#\\#(.*)*|(.*)(?:[\\n])-+/gmi")
 var header_one := RegEx.create_from_string("^\\#(.*)*|(.*)(?:[\\n])=+/gmi")
 
 var bold_italic := RegEx.create_from_string("\\*\\*\\*(.*)\\*\\*\\*|\\_\\_\\_(.*)\\_\\_\\_/gmi")
-var bold := RegEx.create_from_string("\\*\\*(.*)\\*\\|\\_\\_(.*)\\_\\_*/gmi")
+var bold := RegEx.create_from_string("\\*\\*(.*)\\*\\|\\_\\_(.*)\\_\\_*")
 var italic := RegEx.create_from_string("\\*(.*)\\|\\_(.*)\\_*/gmi")
 
 var underline := RegEx.create_from_string("\\_(.*)\\_/gmi")
@@ -28,20 +28,38 @@ var code_block := RegEx.create_from_string("\\`\\`\\`(.*)\\`\\`\\`/gmi")
 var image := RegEx.create_from_string("!\\[([^\\]]+)\\]\\(([^)]+)\\)/gmi")
 var link := RegEx.create_from_string("\\[([^\\]]+)\\]\\(([^)]+)\\)/gmi")
 
-
+## Make list later
 
 func _ready():
 	var markdown_text = """
-	# Header
-	**Bold** and *italic* text.
-	[Link](https://example.com)
-	- List item 1
-	- List item 2
-	"""
+# Header
+**Bold** and *italic* text.
+[Link](https://example.com)
+- List item 1
+- List item 2
+"""
 	var bbcode_text = markdown_to_bbcode(markdown_text)
 	print(bbcode_text)
 
-func markdown_to_bbcode(markdown_text):
+func markdown_to_bbcode(markdown: String):
 	# https://github.com/feralhosting/feralhosting.github.io/blob/master/b2m/index.html
+	markdown = header_six.sub(markdown, "[size=90%]$1[/size]", true)
+	markdown = header_five.sub(markdown, "[size=105%]$1[/size]", true)
+	markdown = header_four.sub(markdown, "[size=120%]$1[/size]", true)
+	markdown = header_three.sub(markdown, "[size=135%]$1[/size]", true)
+	markdown = header_two.sub(markdown, "[size=150%]$1[/size][br][hr]", true)
+	markdown = header_one.sub(markdown, "[size=170%]$1[/size][br][hr]", true)
 	
+	markdown = bold.sub(markdown, "[b]$1$2[/b]", true)
+	markdown = italic.sub(markdown, "[i]$1$2[/i]", true)
 	
+	markdown = underline.sub(markdown, "[u]$1[/u]", true)
+	markdown = strikethrough.sub(markdown, "[s]$1[/s]", true)
+	
+	markdown = superscript.sub(markdown, "[sup]$1$2[/sup]", true)
+	markdown = subscript.sub(markdown, "[sub]$1$2[/sub]", true)
+	
+	markdown = quote.sub(markdown, "[quote]$1$2[/quote]", true)
+	
+	markdown = horizontal_line.sub(markdown, "[hr]", true)
+	return markdown
