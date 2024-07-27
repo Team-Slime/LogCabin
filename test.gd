077118c612a6f5c5380b56ae2556974e8a1f5abe
SlimeyAr <lostbhijaya@gmail.com>
2024-07-27 21:41:58 +0700

    feat: Scene creation DSL

--- test.gd
+++ test.gd
@@ -1,6 +1,12 @@
 extends Control
 class_name MVU
 
+## Planned:
+## - Scene creation DSL (Practically done)
+## - A scene watcher i can wrap around the above to update the scene when the
+## wrapped value changes (it will hide away the deletion, but you can code the
+## addition) (This is mostly to keep the code declarative)
+
 ## Signal for when it's done updating the model
 signal updated
 ## Signal for when it's done transforming the model
@@ -41,3 +47,18 @@ func do_update(message: Message) -> void:
 	var new_view = await _view(message)
 	viewed.emit()
 
+# Real
+func create_node(type, attributes: Dictionary = {}, children: Array[Node] = []):
+	var node: Node = type.new()
+	for child in children:
+		node.add_child(child)
+	for key in attributes.keys():
+		node.set(key, attributes[key])
+	return node
+
+var root_node = create_node(Node2D, {} , [
+	create_node(Sprite2D, {"texture": "res://player.png"}, []),
+	create_node(CharacterBody2D, {"script": "res://player_controller.gd"}, [
+		create_node(CollisionShape2D),
+	])
+])
